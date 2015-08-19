ba0e336691c721457cdef1db8a5f2449016786eb
KDF5000 <kdf5000@163.com>
2015-08-19 21:57:27 +0800

    修改write值

--- SearchData.py
+++ SearchData.py
@@ -12,41 +12,44 @@ sys.setdefaultencoding('UTF-8')
 def fetch_data(name):
     app_request = AppRequest(name)
     data = app_request.get_company_info()
+    print data
     if data is None:
         with open('failed.txt', 'a') as failed:
             failed.write(name+'\n')
     else:
         with open('companies.txt', 'a') as fs:
-            fs.write(data+'\n')
+            fs.write(json.dumps(data)+'\n')
 
     return None
 
-
 if __name__ == '__main__':
-    appRequest = AppRequest('东莞辣妈萌宝')
+    fetch_data('东莞辣妈萌宝')
 
-    su_num = 0
-    err_num = 0
-
-    txt = 'sort.txt'
-    fw = open('res.txt', 'w')
-    fe = open('error.txt', 'w')
-    with open(txt, 'r') as f:
-        for line in f.readlines():
-            print line.strip().encode('gbk')
-            # time.sleep(2)  # 延时2s
-            appRequest.set_company_name(line.strip())
-            res = appRequest.get_company_info()
-            if res is None:
-                err_num += 1
-                print line.strip().encode('gbk'), 'failed'
-                fe.write(line.strip()+'\n')
-            else:
-                su_num += 1
-                fw.write(json.dumps(res)+'\n')
-            print 'su:', su_num, 'err_num:', err_num
-    fw.close()
-    fe.close()
+# if __name__ == '__main__':
+#     appRequest = AppRequest('东莞辣妈萌宝')
+#
+#     su_num = 0
+#     err_num = 0
+#
+#     txt = 'job.txt'
+#     fw = open('res.txt', 'w')
+#     fe = open('error.txt', 'w')
+#     with open(txt, 'r') as f:
+#         for line in f.readlines():
+#             print line.strip().encode('gbk')
+#             # time.sleep(2)  # 延时2s
+#             appRequest.set_company_name(line.strip())
+#             res = appRequest.()
+#             if res is None:
+#                 err_num += 1
+#                 print line.strip().encode('gbk'), 'failed'
+#                 fe.write(line.strip()+'\n')
+#             else:
+#                 su_num += 1
+#                 fw.write(json.dumps(res)+'\n')
+#             print 'su:', su_num, 'err_num:', err_num
+#     fw.close()
+#     fe.close()
 
 
 
